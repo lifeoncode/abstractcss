4986250cf168cbcc9483ad76f7d8d7f64d64281b
lifeoncode <nduduzo@yellogarden.co.za>
2025-01-18 17:44:37 +0200

    verify classname type before splitting

--- src/js/util.js
+++ src/js/util.js
@@ -1,5 +1,11 @@
-const splitElementClassName = (className, splitFrom='-') => {
-  return className.split(splitFrom);
+const splitElementClassName = (className, splitFrom = "-") => {
+  let splitString = "";
+  if (typeof className === "string") {
+    splitString = className.split(splitFrom);
+  }
+
+  return splitString;
+  // return className.split(splitFrom);
 };
 
 export default splitElementClassName;
