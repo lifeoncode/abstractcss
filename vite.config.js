de8b45bcecabcc4744ecaca7e0c74d041114dec4
lifeoncode <nduduzo@yellogarden.co.za>
2025-02-02 14:56:15 +0200

    new config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,21 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
-export default defineConfig(({ mode }) => {
-  const isCSSBuild = mode === "css";
-  return {
-    build: {
-      lib: {
-        entry: isCSSBuild ? "./src/scss/index.scss" : "./src/js/main.js",
-        name: isCSSBuild ? "abstractcss" : "abstractcss_js",
-        fileName: (format) => `abstractcss.${isCSSBuild ? "css" : "js"}`,
-        formats: isCSSBuild ? ["es"] : ["es", "umd"],
-      },
-      rollupOptions: {
-        output: {
-          assetFileNames: "[name].[ext]",
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.js"),
+      name: "AbstractCSS",
+      fileName: "abstractcss",
+      formats: ["es", "umd"],
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "style.css") return "abstractcss.css";
+          return assetInfo.name;
         },
       },
     },
-    css: {
-      postcss: "./postcss.config.js",
-    },
-  };
+  },
 });
